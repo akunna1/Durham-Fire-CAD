f0d3994df3568228336d185b3e33dfe228580e84
Akunna Onyekachi <akunna1mail@gmail.com>
2025-12-14 09:30:30 -0500

    updated navbar a bit

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,10 +1,17 @@
 "use client";
 
+import { useState } from "react";
+
 export default function Navbar() {
+  const [isRunning, setIsRunning] = useState(false);
+
   return (
     <nav className="w-full py-5 flex items-center justify-end px-6 border-b border-gray-800">
-      <button className="bg-red-600 hover:bg-red-800 px-4 py-2 rounded-md text-sm font-bold">
-        Add Incident
+      <button
+        onClick={() => setIsRunning(!isRunning)}
+        className="bg-red-600 hover:bg-red-800 px-4 py-2 rounded-md text-sm font-bold transition-transform duration-200 hover:-translate-y-1"
+      >
+        {isRunning ? "End CAD" : "Run CAD"}
       </button>
     </nav>
   );
